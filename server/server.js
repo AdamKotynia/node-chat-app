e8a0408c8fc8a48e8602edfd45c57271cc9cdf1a
AdamKotynia <adam.kotynia91@gmail.com>
2019-02-17 21:44:58 +0100

    update server/server.js in order to correct the error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -12,5 +12,3 @@ app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
